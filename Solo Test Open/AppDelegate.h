8173c7236326b922b7cd69bff4cd7fb48232660f
Efe Ertugrul <efeertugrul@mac.local>
2015-05-18 11:58:05 +0300

    Initial Commit

---
+++ Solo Test Open/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Solo Test Open
+//
+//  Created by efe ertugrul on 18/05/15.
+//  Copyright (c) 2015 efe ertugrul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Solo Test Open/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Solo Test Open
+//
+//  Created by efe ertugrul on 18/05/15.
+//  Copyright (c) 2015 efe ertugrul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
